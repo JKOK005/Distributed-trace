9b976b60831202aed73a92dfd542510f08bd5bb0
johan.kok <johan.kok@grabtaxi.com>
2019-08-04 10:02:05 +0800

    Removed the concept of transmitter node in main
    Removed validation for transmitter node in main

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	pkg "Distributed-trace/pkg/service/v1"
 	"flag"
 	"fmt"
-	"log"
 	"sync"
 )
 
@@ -18,32 +17,14 @@ const (
 )
 
 var (
-	node_type    *string
 	node_addr    *string
 	node_port 	 *int
 )
 
-func validateAllowableNodeTypes(n string) bool {
-	switch n {
-	case "worker", "transmitter":
-		return true
-	default:
-		return false
-	}
-}
-
 func init() {
-	node_type = flag.String("type", "worker", "Defines the node type")
 	node_addr = flag.String("address", "localhost", "Public node address")
 	node_port = flag.Int("port", 8000, "Public node port")
 	flag.Parse()
-
-	if !validateAllowableNodeTypes(*node_type) {
-		panic(fmt.Sprintf("Node type not allowed: %s", *node_type))
-	}
-
-	log.SetPrefix(fmt.Sprintf("%s (%s:%d): ", *node_addr, *node_type, *node_port))
-	log.Println("Starting node as type", *node_type)
 }
 
 func main() {
