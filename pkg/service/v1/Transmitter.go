18074ced0c19c014f8dc3c98b8e0ce8e690dcf28
johan.kok <johan.kok@grabtaxi.com>
2019-08-04 09:59:37 +0800

    Renamed heartbeatnode class to heartbeat
    Transmitter now sends data straight to Kafka
    - We are removing the concept of individual nodes needing to be transmitters before they can run the logic that sends to sink

--- pkg/service/v1/Transmitter.go
+++ pkg/service/v1/Transmitter.go
@@ -1,50 +1,24 @@
 package v1
 
 import (
-	"fmt"
-	"math/rand"
 	pb "Distributed-trace/pkg/api/proto"
 )
 
-type TransmitterNode struct {
-	ReportChannel 	chan *pb.TraceReport
-}
+type TransmitterNode struct {}
 
-var (
-	sinks_path = "sinks"
-)
-
-func (t TransmitterNode) getFullPath(from_path string) (string) {
-	if from_path == "" {return fmt.Sprintf("/%s/%s", root_path_zk, sinks_path)}
-	return fmt.Sprintf("/%s/%s/%s", root_path_zk, sinks_path, from_path)
-}
-
-func (t TransmitterNode) getSinkNodeAddrs() ([]string, error) {
+func (t TransmitterNode) dispatch(*pb.TraceReport) error {
 	/*
-		Gets a Sink Node Address dynamically from ZK cluster
-	*/
-	client, err := newClient()
-	if err != nil {return nil, err}
-	if node_paths, err := client.GetNodePaths(t.getFullPath("")); err == nil {
-		return node_paths, nil
-	}else {return nil, err}
-}
-
-func (t TransmitterNode) selectAddr(addrs []string) (string) {
-	/*
-		Selects a sink node address out of all addresses
-	*/
-	return addrs[rand.Intn(len(addrs))]
-}
-
-func (t TransmitterNode) relayToSink(report *pb.TraceReport) error {
-	/*
-		Attempts to send received msgs to Sink node
-		Sink node must be registered in ZK for discovery
+		Attempts to send received msgs to Kafka
 	*/
 	return nil
 }
 
 func (t TransmitterNode) Start() {
-
+	for{
+		select {
+		case <- reportChannel:
+			payload := <- reportChannel
+			go t.dispatch(payload)
+		}
+	}
 }
