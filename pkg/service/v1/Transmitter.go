3491d3f8f954fce102dda31bd275e10e1e692618
johan.kok <johan.kok@grabtaxi.com>
2019-08-05 13:33:28 +0800

    Implemented send to Kafka in Transmitter class

--- pkg/service/v1/Transmitter.go
+++ pkg/service/v1/Transmitter.go
@@ -7,17 +7,24 @@ import (
 
 type TransmitterNode struct {}
 
+var (
+	kafkaClient *Kclient
+)
+
 func (t TransmitterNode) dispatch(payload *pb.TraceReport) error {
 	/*
 		Attempts to send received msgs to Kafka
 	*/
-	log.Println(payload.ResponseTiming)
-	return nil
+	return kafkaClient.Dispatch(payload)
 }
 
 func (t TransmitterNode) Start() {
+	if kClient, err := newKafkaProducerClient(); err != nil {
+		log.Fatal(err)
+	}else {kafkaClient = kClient}
+
 	for{
-	 	payload := <- reportChannel
+		payload := <- reportChannel
 		go t.dispatch(payload)
 	}
 }
